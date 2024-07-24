5a5ac0ab3de3e45055eeb639aaf1ec37f7d4f58a
Vikcarvalhos <viktorcarvalhos@gmail.com>
2024-07-24 13:25:52 -0300

    first commit

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,8 @@
+function sortear(){
+    let quantity = document.getElementById('quantidade').value;
+    let fromRange = document.getElementById('de').value;
+    let toRange = document.getElementById('ate').value;
+
+    let numbers = [];
+    
+}
